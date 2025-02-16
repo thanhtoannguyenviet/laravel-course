290e3380801c30589e35ec66a420bc0a2d57e7f7
Thanh Toan <thanhtoan.nguyenviet@gmail.com>
2025-02-16 13:49:32 +0700

    step1-set-up-error-and-flash-messages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,10 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Task List App</title>
+    <style>
+        @yield('styles')
+        .alert-success {
+            color: green;
+        }
+    </style>
 </head>
 <body>
     <h1>@yield('title')</h1>
     <div>
+        @if (session()->has('success'))
+            <div class="alert alert-success">
+                {{ session()->get('success') }}
+            </div>
+        @endif
         @yield('content')
     </div>
 </body>
